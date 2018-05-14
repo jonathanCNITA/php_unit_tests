cef2582081866722497a8afce76ca1ce67fcc949
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-14 15:00:00 +0200

    Add some tests with -30 100 0 and 170 as percent comm

--- tests/Unit/DonationFeeTest.php
+++ tests/Unit/DonationFeeTest.php
@@ -18,10 +18,8 @@ class DonationFeeTest extends TestCase
     {
         // Etant donné une donation de 100 et commission de 10%
         $donationFees = new DonationFee(100, 10);
-
         // Lorsque qu'on appel la méthode getCommissionAmount()
         $actual = $donationFees->getCommissionAmount();
-
         // Alors la Valeur de la commission doit être de 10
         $expected = 10;
         $this->assertEquals($expected, $actual);
@@ -34,5 +32,43 @@ class DonationFeeTest extends TestCase
         // Alors la Valeur de la commission doit être de 20
         $expected = 20;
         $this->assertEquals($expected, $actual);
+
+
+        // Etant donné une donation de 100 et commission de -30%
+        $donationFees = new DonationFee(100, -30);
+        // Lorsque qu'on appel la méthode getCommissionAmount()
+        $actual = $donationFees->getCommissionAmount();
+        // Alors la Valeur de la commission doit être de 30
+        $expected = 30;
+        $this->assertEquals($expected, $actual);
+
+
+        // Etant donné une donation de 100 et commission de 100%
+        $donationFees = new DonationFee(100, 100);
+        // Lorsque qu'on appel la méthode getCommissionAmount()
+        $actual = $donationFees->getCommissionAmount();
+        // Alors la Valeur de la commission doit être de 100
+        $expected = 100;
+        $this->assertEquals($expected, $actual);
+
+
+        // Etant donné une donation de 100 et commission de 0%
+        $donationFees = new DonationFee(100, 0);
+        // Lorsque qu'on appel la méthode getCommissionAmount()
+        $actual = $donationFees->getCommissionAmount();
+        // Alors la Valeur de la commission doit être de 100
+        $expected = 0;
+        $this->assertEquals($expected, $actual);
+
+
+        // Etant donné une donation de 100 et commission de 170%
+        $donationFees = new DonationFee(100, 170);
+        // Lorsque qu'on appel la méthode getCommissionAmount()
+        $actual = $donationFees->getCommissionAmount();
+        // Alors la Valeur de la commission doit être de 100
+        $expected = 170;
+        $this->assertEquals($expected, $actual);
+
+
     }
 }
