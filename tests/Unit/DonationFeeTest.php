15786ff7d8b6ee9359b5d80e7460ae0e0304da05
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-14 14:50:31 +0200

    Add an assertion with 100 and 20 expect 20

--- tests/Unit/DonationFeeTest.php
+++ tests/Unit/DonationFeeTest.php
@@ -26,14 +26,13 @@ class DonationFeeTest extends TestCase
         $expected = 10;
         $this->assertEquals($expected, $actual);
 
-        // Etant donné une donation de 100 et commission de 10%
-        $donationFees = new DonationFee(100, 20);
 
+        // Etant donné une donation de 100 et commission de 20%
+        $donationFees = new DonationFee(100, 20);
         // Lorsque qu'on appel la méthode getCommissionAmount()
         $actual = $donationFees->getCommissionAmount();
-
-        // Alors la Valeur de la commission doit être de 10
-        $expected = 30;
+        // Alors la Valeur de la commission doit être de 20
+        $expected = 20;
         $this->assertEquals($expected, $actual);
     }
 }
