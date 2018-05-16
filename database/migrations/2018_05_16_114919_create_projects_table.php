4f5c180af977401bafd29ef2574ed560da06bfd8
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-16 17:04:35 +0200

    create the migration file for projects

---
+++ database/migrations/2018_05_16_114919_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('auth')->default('user');
+            $table->string('title');
+            $table->string('resume');
+            $table->text('content');
+            $table->string('imageurl');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
