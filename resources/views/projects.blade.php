d32c1c9906742b004c97ce9bf0b0d3d94502f693
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-17 10:31:48 +0200

    Implement the @extends & @section

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,84 +1,14 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('template')
 
-        <title>Laravel</title>
+@section('titre')
+    <h1>Liste des projets</h1>
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            .img-project{
-                width: 100%;
-                height: 100%;
-                max-width: 100px;
-                max-height: 100px;
-                border-radius: 50%;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Liste des projets</h1>
-        @foreach($projects as $project)
-            <h2>{{ $project->title }}</h2>
-            <p>{{ $project->resume }}</p>
-            <img class="img-project" src="{{ $project->imageurl }}" alt="imageProject" >
-            <a href="{{ route('ficheProjet', [$project->id]) }}">Voir le projet</a>
-        @endforeach
-    </body>
-</html>
+@section('contenu')
+    @foreach($projects as $project)
+        <h2>{{ $project->title }}</h2>
+        <p>{{ $project->resume }}</p>
+        <img class="img-project" src="{{ $project->imageurl }}" alt="imageProject" >
+        <a href="{{ route('ficheProjet', [$project->id]) }}">Voir le projet</a>
+    @endforeach
+@endsection
