984b2105ff8a66a299f78826d08d6a1d7e2fba95
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-17 13:43:28 +0200

    Add a button donate on project page

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -8,4 +8,6 @@
         <p>{{ $project->created_at }} by <strong>{{ $project->auth }}</strong></p>
         <p>{{ $project->content }}</p>
         <img class="img-project" src="{{ $project->imageurl }}" alt="imageProject">
+        <h2>Contribuer</h2>
+        <a href="/donate" <button id="contribuer" class="btn btn-info">faire un don</button></a>
 @endsection
