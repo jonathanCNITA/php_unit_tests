390bbd06b05eb0a0121328e0db09c6123f20391a
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-17 17:11:39 +0200

    add factory test

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -33,4 +33,16 @@ class ProjectTest extends TestCase
         $response->assertSee($toSearch);
     }
 
+     /**
+     *  TEST AVEC LES DATAS DU FACTORY POUR PROJECT
+     */
+
+    public function testPresenceOfH2InProjects()
+    {
+        $project = factory(\App\Project::class, 100)->create()->random();
+        $response = $this->get('/projects');
+        $toSearch = '<h2>' . $project->title . '</h2>';
+        $response->assertSee($toSearch);
+    }
+
 }
