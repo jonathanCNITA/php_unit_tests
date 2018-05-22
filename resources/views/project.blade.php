846290d5a69220ad4988ffdefe948e4457a3d4d7
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-22 13:35:34 +0200

    Update auth to autor

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenu')
-        <p>{{ $project->created_at }} by <strong>{{ $project->auth }}</strong></p>
+        <p>{{ $project->created_at }} by <strong>{{ $project->autor }}</strong></p>
         <p>{{ $project->content }}</p>
         <img class="img-project" src="{{ $project->imageurl }}" alt="imageProject">
         <h2>Contribuer</h2>
