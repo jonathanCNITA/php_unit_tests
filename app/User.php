8eb0ee52b5839733e66c7e0baab485e86f8e7c18
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-22 13:34:20 +0200

    Make relation with Project

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function project()
+    {
+        return $this->hasMany('App\Project');
+    }
 }
