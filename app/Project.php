8e1c15ab2d486fef2affcea57928e3db22950cfe
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-22 13:34:05 +0200

    Make relation with User

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+protected $fillable = [
+    'autor', 'title', 'resume', 'content', 'imageurl', 'user_id'
+];
+
+    public function user() 
+    {
+        return $this->belongsTo('App\User');
+    }
 }
