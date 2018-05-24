05afecd3be9cde03fc00a3f6cf75911478f501aa
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-05-24 11:49:35 +0200

    Show edit button if author is user

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -10,4 +10,7 @@
         <img class="img-project" src="{{ $project->imageurl }}" alt="imageProject">
         <h2>Contribuer</h2>
         <a href="/donate" <button id="contribuer" class="btn btn-info">faire un don</button></a>
+        @if($project->user_id == $user->id)
+            <a href="/project/edit/{{ $project->id }}" <button id="edit" class="btn btn-info">edit</button></a>
+        @endif
 @endsection
